b99ca70b825d1592df9738a81ea644c46afb153a
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-01-06 13:34:46 -0800

    Dispatched tasks of ProductService.

--- dispatcher.ts
+++ dispatcher.ts
@@ -1,16 +1,30 @@
 import { INTERVAL_MS } from "./params";
 import { SERVICE_CLIENT } from "./service_client";
 import {
-  listGcsFileDeleteTasks,
+  listCoverImageDeletingTasks,
+  listVideoContainerCreatingTasks,
+  listVideoContainerDeletingTasks,
+  processCoverImageDeletingTask,
+  processVideoContainerCreatingTask,
+  processVideoContainerDeletingTask,
+} from "@phading/product_service_interface/show/node/client";
+import {
+  listGcsFileDeletingTasks,
   listMediaFormattingTasks,
-  listR2KeyDeleteTasks,
+  listR2KeyDeletingTasks,
+  listStorageEndRecordingTasks,
+  listStorageStartRecordingTasks,
   listSubtitleFormattingTasks,
+  listUploadedRecordingTasks,
   listVideoContainerSyncingTasks,
   listVideoContainerWritingToFileTasks,
-  processGcsFileDeleteTask,
+  processGcsFileDeletingTask,
   processMediaFormattingTask,
-  processR2KeyDeleteTask,
+  processR2KeyDeletingTask,
+  processStorageEndRecordingTask,
+  processStorageStartRecordingTask,
   processSubtitleFormattingTask,
+  processUploadedRecordingTask,
   processVideoContainerSyncingTask,
   processVideoContainerWritingToFileTask,
 } from "@phading/video_service_interface/node/client";
@@ -33,14 +47,59 @@ export class Dispatcher {
 
   private async dispatch(): Promise<void> {
     await Promise.all([
+      this.dispatchVideoContainerCreatingTasks(),
+      this.dispatchVideoContainerDeletingTasks(),
+      this.dispatchCoverImageDeletingTasks(),
       this.dispatchWritingToFileTasks(),
       this.dispatchSyncingTasks(),
+      this.dispatchUploadedRecordingTasks(),
       this.dispatchMediaFormattingTasks(),
       this.dispatchSubtitleFormattingTasks(),
-      this.dispatchGcsFileDeleteTasks(),
-      this.dispatchR2KeyDeleteTasks(),
+      this.dispatchStorageStartRecordingTasks(),
+      this.dispatchStorageEndRecordingTasks(),
+      this.dispatchGcsFileDeletingTasks(),
+      this.dispatchR2KeyDeletingTasks(),
     ]);
     this.setTimeout(() => this.dispatch(), INTERVAL_MS);
+  }
+
+  private async dispatchVideoContainerCreatingTasks(): Promise<void> {
+    let { tasks } = await listVideoContainerCreatingTasks(
+      this.serviceClient,
+      {},
+    );
+    await Promise.all(
+      tasks.map((task) =>
+        processVideoContainerCreatingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
+    );
+  }
+
+  private async dispatchVideoContainerDeletingTasks(): Promise<void> {
+    let { tasks } = await listVideoContainerDeletingTasks(
+      this.serviceClient,
+      {},
+    );
+    await Promise.all(
+      tasks.map((task) =>
+        processVideoContainerDeletingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
+    );
+  }
+
+  private async dispatchCoverImageDeletingTasks(): Promise<void> {
+    let { tasks } = await listCoverImageDeletingTasks(this.serviceClient, {});
+    await Promise.all(
+      tasks.map((task) =>
+        processCoverImageDeletingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
+    );
   }
 
   private async dispatchWritingToFileTasks(): Promise<void> {
@@ -49,9 +108,11 @@ export class Dispatcher {
       {},
     );
     await Promise.all(
-      tasks.map((task) => {
-        processVideoContainerWritingToFileTask(this.serviceClient, task);
-      }),
+      tasks.map((task) =>
+        processVideoContainerWritingToFileTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
     );
   }
 
@@ -61,45 +122,83 @@ export class Dispatcher {
       {},
     );
     await Promise.all(
-      tasks.map((task) => {
-        processVideoContainerSyncingTask(this.serviceClient, task);
-      }),
+      tasks.map((task) =>
+        processVideoContainerSyncingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
+    );
+  }
+
+  private async dispatchUploadedRecordingTasks(): Promise<void> {
+    let { tasks } = await listUploadedRecordingTasks(this.serviceClient, {});
+    await Promise.all(
+      tasks.map((task) =>
+        processUploadedRecordingTask(this.serviceClient, task).catch((e) => {}),
+      ),
     );
   }
 
   private async dispatchMediaFormattingTasks(): Promise<void> {
     let { tasks } = await listMediaFormattingTasks(this.serviceClient, {});
     await Promise.all(
-      tasks.map((task) => {
-        processMediaFormattingTask(this.serviceClient, task);
-      }),
+      tasks.map((task) =>
+        processMediaFormattingTask(this.serviceClient, task).catch((e) => {}),
+      ),
     );
   }
 
   private async dispatchSubtitleFormattingTasks(): Promise<void> {
     let { tasks } = await listSubtitleFormattingTasks(this.serviceClient, {});
     await Promise.all(
-      tasks.map((task) => {
-        processSubtitleFormattingTask(this.serviceClient, task);
-      }),
+      tasks.map((task) =>
+        processSubtitleFormattingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
     );
   }
 
-  private async dispatchGcsFileDeleteTasks(): Promise<void> {
-    let { tasks } = await listGcsFileDeleteTasks(this.serviceClient, {});
+  private async dispatchStorageStartRecordingTasks(): Promise<void> {
+    let { tasks } = await listStorageStartRecordingTasks(
+      this.serviceClient,
+      {},
+    );
     await Promise.all(
-      tasks.map((task) => {
-        processGcsFileDeleteTask(this.serviceClient, task);
-      }),
+      tasks.map((task) =>
+        processStorageStartRecordingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
     );
   }
 
-  private async dispatchR2KeyDeleteTasks(): Promise<void> {
-    let { tasks } = await listR2KeyDeleteTasks(this.serviceClient, {});
+  private async dispatchStorageEndRecordingTasks(): Promise<void> {
+    let { tasks } = await listStorageEndRecordingTasks(this.serviceClient, {});
     await Promise.all(
-      tasks.map((task) => {
-        processR2KeyDeleteTask(this.serviceClient, task);
-      }),
+      tasks.map((task) =>
+        processStorageEndRecordingTask(this.serviceClient, task).catch(
+          (e) => {},
+        ),
+      ),
+    );
+  }
+
+  private async dispatchGcsFileDeletingTasks(): Promise<void> {
+    let { tasks } = await listGcsFileDeletingTasks(this.serviceClient, {});
+    await Promise.all(
+      tasks.map((task) =>
+        processGcsFileDeletingTask(this.serviceClient, task).catch((e) => {}),
+      ),
+    );
+  }
+
+  private async dispatchR2KeyDeletingTasks(): Promise<void> {
+    let { tasks } = await listR2KeyDeletingTasks(this.serviceClient, {});
+    await Promise.all(
+      tasks.map((task) =>
+        processR2KeyDeletingTask(this.serviceClient, task).catch((e) => {}),
+      ),
     );
   }
 }
